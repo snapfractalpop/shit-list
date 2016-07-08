75fdd820f1cf9eccc0df0476fefb96653a36cec7
Matthew Kevins <mkevins@gmail.com>
2016-07-08 13:36:57 -0400

    Generate smaller zip in build script and update version in manifest.

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 browserify js/backgroundMain.js | uglifyjs -cm > js/backgroundBundle.min.js
 browserify js/contentScriptMain.js | uglifyjs -cm > js/contentScriptBundle.min.js
+zip ../shitList.zip ./manifest.json
+zip -r ../shitList.zip ./css
+zip -r ../shitList.zip ./images
+zip -r ../shitList.zip ./js/libs
+zip ../shitList.zip ./js/backgroundBundle.min.js
+zip ../shitList.zip ./js/contentScriptBundle.min.js
