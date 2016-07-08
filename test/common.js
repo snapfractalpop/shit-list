bf2347ccd8ad57c94566225a04fc55e8df16c97d
Matthew Kevins <mkevins@gmail.com>
2016-07-08 11:03:27 -0400

    Setup mocha testing.

---
+++ test/common.js
@@ -0,0 +1,15 @@
+global.chai = require('chai');
+global.sinon = require('sinon');
+require('sinon-as-promised');
+global.sinonChai = require('sinon-chai');
+global.chaiJq = require('chai-jq');
+global.chaiAsPromised = require('chai-as-promised');
+global.expect = chai.expect;
+global.chrome = require('chrome-stub');
+global.URL = require('url-parse');
+chai.use(sinonChai);
+chai.use(chaiJq);
+chai.use(chaiAsPromised);
+
+global.jsdom = require('jsdom');
+global.$ = require('jquery')(jsdom.jsdom().parentWindow);
