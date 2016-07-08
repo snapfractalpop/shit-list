411d72ccbff815dbd191e13e90609d3b5635b636
Matthew Kevins <mkevins@gmail.com>
2016-07-08 11:52:55 -0400

    Add shitList function.

--- js/shitList.js
+++ js/shitList.js
@@ -1,2 +1,14 @@
-$(function () {
-});
+var shitList = {};
+
+shitList.shitList = function (hostname) {
+  var values = {};
+  values[hostname] = true;
+
+  return new Promise(function (resolve, reject) {
+    chrome.storage.sync.set(values, function (results) {
+      resolve();
+    });
+  });
+}.bind(shitList);
+
+module.exports = shitList;
