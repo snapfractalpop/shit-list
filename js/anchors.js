f9e35508756120f1760cb69f047ecb0fa106dc5b
Matthew Kevins <mkevins@gmail.com>
2016-07-08 12:05:19 -0400

    Add getHostname function.

--- js/anchors.js
+++ js/anchors.js
@@ -8,4 +8,13 @@ anchors.getShitList = function (shitList) {
   return this._shitList;
 }.bind(anchors);
 
+anchors.getHostname = function ($a) {
+  try {
+    var url = new URL($a.prop('href'));
+  } catch (e) {
+    return '';
+  }
+  return url.hostname;
+}.bind(anchors);
+
 module.exports = anchors;
