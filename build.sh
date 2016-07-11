fa62b14624da504bba11354ff8ac819cc531a183
Matthew Kevins <mkevins@gmail.com>
2016-07-11 15:43:36 -0400

    Keep build output in project directory; update .gitignore file.

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 browserify js/backgroundMain.js | uglifyjs -cm > js/backgroundBundle.min.js
 browserify js/contentScriptMain.js | uglifyjs -cm > js/contentScriptBundle.min.js
-zip ../shitList.zip ./manifest.json
-zip -r ../shitList.zip ./css
-zip -r ../shitList.zip ./images
-zip -r ../shitList.zip ./js/libs
-zip ../shitList.zip ./js/backgroundBundle.min.js
-zip ../shitList.zip ./js/contentScriptBundle.min.js
+rm -f build/shitList-release.zip
+mkdir -p build
+zip build/shitList-release.zip ./manifest.json
+zip -r build/shitList-release.zip ./css
+zip -r build/shitList-release.zip ./images
+zip -r build/shitList-release.zip ./js/libs
+zip build/shitList-release.zip ./js/backgroundBundle.min.js
+zip build/shitList-release.zip ./js/contentScriptBundle.min.js
